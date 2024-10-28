4b4ef3f3c70fc42619d747365e8491802aff52f8
Joao <jmmsp2003@hotmail.com>
2024-10-28 18:27:20 -0300

    first commit

---
+++ tarefas/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+def lista_tarefas(request):
+  tarefas = ["Estudar Django", "Criar um projeto", "Revisar o conteúdo"]
+  return render(request, 'lista_tarefas.html', {'tarefas':
+  tarefas})
+
