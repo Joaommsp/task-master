32a8991e241483e472adc3d0c1d1f74f232073ad
João Marcos Melo Santos Pinto <212.14.017@EDU.gruposete.local>
2024-10-28 22:26:55 -0300

    style: form interface

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from .models import Tarefa
 # Create your views here.
 
 from django.shortcuts import render
 def lista_tarefas(request):
-  tarefas = ["Estudar Django", "Criar um projeto", "Revisar o conteúdo"]
+  tarefas = Tarefa.objects.all()
   return render(request, 'lista_tarefas.html', {'tarefas':
   tarefas})
 
